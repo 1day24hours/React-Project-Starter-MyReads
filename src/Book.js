120c7174a5acba39c6b8d1ce6e70ea9ff09a9bde
Felix Chen <chen.felix.xi@gmail.com>
2018-06-23 22:24:36 +0800

    1st commit - Mission Completed

---
+++ src/Book.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+class Book extends React.Component {
+    render() {
+        // initialize the shelf's state
+        let shelf ='';
+        if (this.props.book.shelf) {
+            shelf = this.props.book.shelf;
+        } else {
+            shelf = 'none';
+        }
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, 
+                            backgroundImage: `url('${this.props.book.imageLinks.thumbnail}')`}}></div>
+                        <div className="book-shelf-changer">
+                            <select 
+                                //pass shelf's state to value
+                                value={shelf}
+                                onChange={(event) => this.props.updateShelf(this.props.book, event.target.value) }
+                            >
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{this.props.book.title}</div>
+                    {this.props.book.authors.map((author) => (
+                        <div className='book-authors' 
+                        key={this.props.book.author}>{author}</div>
+                    ))}
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Book
---
+++ src/BookShelf.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Book from './Book'
+import BooksApp from './App'
+
+class BookShelf extends React.Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => (
+                            <Book
+                            key={book.id}
+                            book={book}
+                            updateShelf={this.props.updateShelf}
+                            />
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookShelf
